c8610504b5c62dffd9355f3454185c525b705bb4
VictoriaRaitses <victoria.raitses@gmail.com>
2020-08-19 20:11:39 +0200

    initial commit

---
+++ maximum_segments_for_point/src/MaxSegmentsFinder.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MaxSegmentsFinder {
+
+    public int getMaxSegments(List<Integer> points, List<Segment> segments){
+        Map<Integer,Integer> po = new HashMap<>();
+        for (int i = 0; i < points.size(); i++) {
+            for (int j = 0; j < segments.size(); j++) {
+                if()
+            }
+        }
+    }
+}
---
+++ maximum_segments_for_point/src/Segment.java
@@ -0,0 +1,9 @@
+public class Segment {
+    private int left;
+    private int right;
+
+    public Segment(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+}
