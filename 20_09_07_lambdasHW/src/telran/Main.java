3f3f76d5539cb81b143c8eaf238d405b5dcb7255
VictoriaRaitses <victoria.raitses@gmail.com>
2020-09-06 18:16:07 +0200

    unique numbers

---
+++ 20_09_07_lambdasHW/src/telran/Main.java
@@ -0,0 +1,22 @@
+package telran;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        //первое задание
+        Predicate<UniqueNumbers> p = uniqueNumbers -> uniqueNumbers.test(1, 6, 4);
+        System.out.println(p.test(new UniqueNumbers()));
+
+        //второе задание
+        Function<Integer, Integer> x = num -> num * 2;
+        Function<Integer, Integer> f = x;
+        System.out.println(f.compose(x).apply(4));
+
+        Function<Integer, Integer> y = num -> num + 3;
+        Function<Integer, Integer> g = y;
+        System.out.println(g.compose(y).apply(4));
+
+    }
+}
---
+++ 20_09_07_lambdasHW/src/telran/TernaryIntPredicate.java
@@ -0,0 +1,7 @@
+package telran;
+
+import java.util.function.Predicate;
+
+public interface TernaryIntPredicate {
+   public boolean test(int arg1, int arg2, int arg3);
+}
